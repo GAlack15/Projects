005cc150af29615be7e83824f8f03ac6f239d11c
GAlack15 <109077380+GAlack15@users.noreply.github.com>
2023-02-17 17:30:47 -0500

    update

--- Java/Banking app project/src/Record.java
+++ Java/Banking app project/src/Record.java
@@ -1,17 +1,15 @@
-// Final Project: Banking Application
-// Project Week: ThanksGiving
 //Record Page
 //Java Class
 // Gabriel Lackie
 
 import java.util.ArrayList;
 public class Record extends ArrayList<Record> {
-   public int idNumber = -1;
+    public int idNumber = -1;
     public String Name = " ";
-   public double currentChecking = -1;
-   public double  currentSavings = -1;
-   public double percentage = -1;
-    ArrayList<Record> records = new ArrayList<>();
+    public double currentChecking = -1;
+    public double  currentSavings = -1;
+    public double percentage = -1;
+
     public Record (int idNumber, String Name, double currentChecking, double currentSavings, double percentage){
         this.idNumber = idNumber;
         this.Name = Name;
@@ -26,56 +24,18 @@ public class Record extends ArrayList<Record> {
         percentage = -1;
         currentSavings = -1;
     }
- static double Deposit_withdrawl (double balance, double transaction){
-      return balance + transaction;
- }
-static double Calculate_Interest(double interestRate, double amount) {
-        return interestRate * amount;
-}
-  /*  // getters
-    public int getidNumber() {
-
-        return idNumber;
+    static double Deposit_withdrawl (double balance, double transaction){
+        return balance + transaction;
     }
-    public String GetName(){
+    static double Calculate_Interest(double interestRate, double amount) {
+        if (amount > -1){
 
-        return Name;
-    }
-    public double getcurrentChecking(){
-
-        return currentChecking;
-    }
-    public double getcurrentSavings(){
-
-        return currentSavings;
-    }
-    public double getpercentage(){
-
-        return percentage;
-    }
-    // setters
-    public void setidNumber(int idNumber){
-
-        this.idNumber = idNumber;
+            return interestRate * amount;
+        }
+        else{
+            System.out.println("your balance is in the negative so a zero is returned");
+            return 0;
+        }
     }
 
-    public void setName(String Name){
-
-        this.Name = Name;
-    }
-    public void getName(){
-        this.Name = Name;
-    }
-    public void setcurrentChecking(double currentChecking){
-        this.currentChecking = currentChecking;
-    }
-    public void setcurrentSavings(double currentSavings){
-
-        this.currentSavings = currentSavings;
-    }
-    public void setpercentage(double percentage){
-
-        this.percentage = percentage;
-    }
-*/
 }
